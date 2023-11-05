e99d2633cf0b89f68c97c724364c9f892b3ca69a
Reduan4u <reduanulhaque13@gmail.com>
2023-11-05 22:29:58 +0600

    2nd commit after json

--- src/Components/Root/Navbar/Navbar.jsx
+++ src/Components/Root/Navbar/Navbar.jsx
@@ -35,11 +35,11 @@ const Navbar = () => {
         <div className="bg-white text-black w-11/12 m-auto rounded-lg">
             <div className="navbar max-w-6xl mx-auto">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <div className="dropdown ">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden ">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52">
                             {links}
                         </ul>
                     </div>
